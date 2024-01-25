6beefd67dbb3b7a20e0c9cfa3f1c785d15db4ec5
Kundan Mani <kundanmani40@gmail.com>
2024-01-25 16:55:11 +0530

    printing system date.

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class main{
+    public static void main(String args[]){
+        System.out.println("Hello Everyone!");
+        System.out.println(new Date());
+
+
+    }
+}
